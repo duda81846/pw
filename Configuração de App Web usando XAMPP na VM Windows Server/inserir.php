37a48d0d70e6ff8abcf56e97861b133af6cf738d
Maria Eduarda Budin <duda81846@gmail.com>
2025-11-24 11:27:51 -0300

    inserir.php

--- Configuração de App Web usando XAMPP na VM Windows Server/inserir.php
+++ Configuração de App Web usando XAMPP na VM Windows Server/inserir.php
@@ -1,35 +1,42 @@
 <?php
- 
+
 $servidor = "localhost";
 $usuario = "root";
 $senha = "";      
 $banco = "agenda";
- 
+
 $conn = mysqli_connect($servidor, $usuario, $senha, $banco);
- 
+
 if (!$conn) {
     die("Conexão falhou: " . mysqli_connect_error());
 }
- 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $telefone = $_POST['telefone'];
-    $insta = $_POST['instagram'];
- 
-    $stmt = $conn->prepare("INSERT INTO dados (nome, email, telefone, instagram) VALUES (?, ?, ?, ?)");
-   
-    $stmt->bind_param("ssss", $nome, $email, $telefone, $instagram);
-   
-    if ($stmt->execute()) {
-        echo "Registro inserido com sucesso!";
+    if (isset($_POST['nome'], $_POST['email'], $_POST['telefone'], $_POST['conta_instagram'])) {
+        $nome = mysqli_real_escape_string($conn, trim($_POST['nome']));
+        $email = mysqli_real_escape_string($conn, trim($_POST['email']));
+        $telefone = mysqli_real_escape_string($conn, trim($_POST['telefone']));
+        $conta_instagram = mysqli_real_escape_string($conn, trim($_POST['conta_instagram']));
+
+        $stmt = $conn->prepare("INSERT INTO usuario (nome, email, telefone, conta_instagram) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $nome, $email, $telefone, $conta_instagram);
+
+
+        if ($stmt->execute()) {
+            echo "Registro inserido com sucesso!";
+        } else {
+            echo "Erro ao inserir registro: " . $stmt->error;
+        }
+
+        $stmt->close();
     } else {
-        echo "Erro ao inserir registro: " . $stmt->error;
+        echo "Por favor, preencha todos os campos.";
     }
- 
-    $stmt->close();
+
     $conn->close();
 }
- 
+
 ?>
-<a href=consulta.html>Voltar para tela inicial
+
+<!-- Link de volta para a tela inicial -->
+<a href="consulta.html">Voltar para tela inicial</a>
