77f2546bebb12a3ce3f3a9ef11aa2710c67c264a
Maria Eduarda Budin <duda81846@gmail.com>
2025-11-24 11:25:46 -0300

    consulta.php

--- Configuração de App Web usando XAMPP na VM Windows Server/consulta.php
+++ Configuração de App Web usando XAMPP na VM Windows Server/consulta.php
@@ -1,49 +1,46 @@
 <html>
 <body>
 <?php
-$expressao= $_POST["expressao"];
-$bd=mysqli_connect("localhost","root","","agenda") or die ("erro!");
- 
-if (isset($_POST ["op"]))
-{
-$op = $_POST ["op"];
-if ($op=="nome")
-$consulta=mysqli_query($bd,"select * from dados where nome='$expressao'");
-if ($op=="email")
-$consulta=mysqli_query($bd,"select * from dados where email='$expressao'");
-if ($op=="telefone")
-$consulta=mysqli_query($bd,"select * from dados where telefone ='$expressao'");
-if ($op=="instagram")
-$consulta=mysqli_query($bd,"select * from dados where instagram ='$expressao'");
-} else
-{
-echo "volte a página e escolha outro campo para a pesquisa";
-exit;
+$expressao = $_POST["expressao"];
+$bd = mysqli_connect("localhost", "root", "", "agenda") or die("erro!");
+
+if (isset($_POST["op"])) {
+    $op = $_POST["op"];
+    if ($op == "nome")
+        $consulta = mysqli_query($bd, "SELECT * FROM usuario WHERE nome='$expressao'");
+    if ($op == "email")
+        $consulta = mysqli_query($bd, "SELECT * FROM usuario WHERE email='$expressao'");
+    if ($op == "telefone")
+        $consulta = mysqli_query($bd, "SELECT * FROM usuario WHERE telefone='$expressao'");
+    if ($op == "conta_instagram")
+        $consulta = mysqli_query($bd, "SELECT * FROM usuario WHERE conta_instagram='$expressao'");
+} else {
+    echo "volte a página e escolha outro campo para a pesquisa";
+    exit;
 }
+
 $reg = mysqli_fetch_array($consulta);
-if ($reg==0)
-{
-echo "Não existem registros para a pesquisa!";
-exit;
+
+if (!$reg) {
+    echo "Não existem registros para a pesquisa!";
+    exit;
 }
-while ($reg!=0)
-{
-$nome = $reg["nome"];
-$email = $reg["email"];
-$telefone = $reg["telefone"];
-$instagram = $reg["instagram"];
- 
-echo "nome: $nome<br>
+
+while ($reg) {
+    $nome = $reg["NOME"];
+    $email = $reg["EMAIL"];
+    $telefone = $reg["TELEFONE"];
+    $conta_instagram = $reg["CONTA_INSTAGRAM"];
+
+    echo "nome: $nome<br>
 email: $email<br>
 telefone: $telefone<br>
-instagram: $instagram<br>";
- 
+instagram: $conta_instagram<br>";
 ?>
-<a href="excluir.php?pl=<?php echo $nome;?>" onclick = "return confirm ('Exclui o registro?');">Excluir</a>
- 
-<a href="alterar.php?pl=<?php echo $nome;?>">Alterar</a><hr>
+<a href="excluir.php?pl=<?php echo $nome; ?>" onclick="return confirm('Exclui o registro?');">Excluir</a>
+<a href="alterar.php?pl=<?php echo $nome; ?>">Alterar</a><hr>
 <?php
-$reg = mysqli_fetch_array($consulta);
+    $reg = mysqli_fetch_array($consulta);
 }
 ?>
 <br><a href="consulta.html">Voltar</a><br>
