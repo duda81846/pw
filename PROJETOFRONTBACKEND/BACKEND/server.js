076c77cbd4d51eb6cebfba0608c8dafa2c1c7ea1
Maria Eduarda Budin <duda81846@gmail.com>
2026-03-19 14:37:08 -0300

    Add files via upload

---
+++ PROJETOFRONTBACKEND/BACKEND/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const porta = 8000;
+
+app.use(cors({
+    origin: "http://127.0.0.1:5500"
+}));
+
+//simulando bd
+let usuarios = [];
+let idAtual = 1;
+
+app.listen(porta, (request, response) => {
+    console.log(`Servidor rodando: http://127.0.0.1.${porta}`)
+});
